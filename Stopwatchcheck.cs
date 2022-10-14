82c5dd82462d2162f1f16b46e9456395fa2927a4
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-14 19:26:37 +0530

    Add files via upload
    stopwatch prog added

---
+++ Stopwatchcheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Day6logicalproblems
+{
+    public class Stopwatchcheck
+    {
+        public static void Stopwatchchec()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 1; i < 10; i++)
+            {
+                Thread.Sleep(i);
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Time Elapsed : {0}", stopwatch.Elapsed);
+
+        }
+    }
+}
