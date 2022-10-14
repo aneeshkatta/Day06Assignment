0ed59a0caa5aaf91919e2c5f84b4d1a20ddea760
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-14 19:25:06 +0530

    Add files via upload
    reverse number prog added

---
+++ ReverseNo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6logicalproblems
+{
+    internal class ReverseNo
+    {
+        
+        public static void revno()
+        {
+            int reverse = 0;
+            Console.WriteLine("Enter a number to reverse: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            while (num != 0)
+            {
+                int rem = num % 10;
+                num = num/10;
+                reverse = reverse * 10 + rem;
+                            }
+            Console.Write("Reversed Number: " + reverse);
+        }
+    }
+}
