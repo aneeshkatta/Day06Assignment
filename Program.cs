128c1abcca2688084d3bca6ad959a30a349aba32
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-14 19:20:33 +0530

    Add files via upload
    common program.cs added for day06 problems

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Day6logicalproblems
+{
+   public class Program
+    {
+        public static void Main()
+        {
+            //uc1_fibannoci.fibanociseries();
+            //PerfectNumber.Perfectnumber();
+            //Primecheck.prime();
+            //ReverseNo.revno();
+            //Stopwatchcheck.Stopwatchchec();
+            Coupon.GenerateCoupon();
+
+
+        }
+    }
+}
